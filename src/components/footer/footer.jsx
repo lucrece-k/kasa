bc39985582757304ec83ea1f18e91ea328b46c9a
kouame lucrèce <kouamelucrece@gmail.com>
2025-03-22 19:47:06 +0100

    update

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,24 +1,16 @@
 import "../footer/footer.scss";
+import lettreKkasa from "../../assets/lettre-k-kasa.png";
+import maisonKasa from "../../assets/maison-kasa.png";
+import lettreSkasa from "../../assets/lettre-s-kasa.png";
+import lettreAkasa from "../../assets/lettre-a-kasa.png";
 function Footer() {
   return (
     <div className="ks-footer">
       <div className="footer-logo">
-        <img src="../assets/lettre-k-kasa.png" alt="lettre k " />
-        <img
-          className="lettre"
-          src="../assets/maison-kasa.png"
-          alt="maison kasa"
-        />
-        <img
-          className="lettre"
-          src="../assets/lettre-s-kasa.png"
-          alt="lettre s "
-        />
-        <img
-          className="lettre"
-          src="../assets/lettre-a-kasa.png"
-          alt="lettre a"
-        />
+        <img src={lettreKkasa} alt="lettre k " />
+        <img className="lettre" src={maisonKasa} alt="maison kasa" />
+        <img className="lettre" src={lettreSkasa} alt="lettre s " />
+        <img className="lettre" src={lettreAkasa} alt="lettre a" />
       </div>
       <p>
         © 2020 Kasa. All
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,13 @@
 import { Link, useLocation } from "react-router-dom";
 import "../header/header.scss";
+import logo from "../../assets/logo.png";
 function Header() {
   const location = useLocation();
 
   return (
     <div className="ks-navbar">
       <div className="ks-logo">
-        <img src="../assets/logo.png" alt="logo de kasa" />
+        <img src={logo} alt="logo de kasa" />
       </div>
       <nav className="ks-lien-nav">
         <Link to="/" className={location.pathname === "/" ? "underline" : ""}>
